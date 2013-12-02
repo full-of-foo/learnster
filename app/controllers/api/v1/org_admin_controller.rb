cc57fcad4cea7bd44c00fc999d3f5a05f1703f39
Anthony <lightweightdevelopment@gmail.com>
2013-12-02 17:42:17 +0000

    OrgAdmin search via range

--- app/controllers/api/v1/org_admin_controller.rb
+++ app/controllers/api/v1/org_admin_controller.rb
@@ -1,76 +1,76 @@
 class Api::V1::OrgAdminController < ApplicationController
-	respond_to :json
-	before_filter :find_org
-	before_filter :require_login
-	
+  respond_to :json
+  before_filter :find_org
+  before_filter :require_login
+  
 
-	def index
-		if search_request?
-			@org_admins = OrgAdmin.search_term(params[:search]) 							if not nested_org_request?(params)
-			@org_admins = OrgAdmin.search_term(params[:search], @org) 				if nested_org_request?(params)
-			@org_admins = OrgAdmin.search_range(params[:created_months_ago]) 	if params[:created_months_ago]
-			@org_admins = OrgAdmin.search_range(params[:updated_months_ago]) 	if params[:updated_months_ago]
-			return @org_admins
-		end
-		
-		@org_admins = nested_org_request?(params) ? @org.admins() : OrgAdmin.all()
+  def index
+    if search_request?
+      @org_admins = OrgAdmin.search_term(params[:search])                             if not nested_org_request?(params)
+      @org_admins = OrgAdmin.search_term(params[:search], @org)                       if nested_org_request?(params)
+      @org_admins = OrgAdmin.search_range(params[:created_months_ago], :created_at)   if params[:created_months_ago]
+      @org_admins = OrgAdmin.search_range(params[:updated_months_ago], :updated_at)  if params[:updated_months_ago]
+      return @org_admins
+    end
+    
+    @org_admins = nested_org_request?(params) ? @org.admins() : OrgAdmin.all()
 
-		if xlsx_request?
-			respond_to do |format|
-				format.xlsx {
-					send_data @org_admins.to_xlsx.to_stream.read, :filename => 'admins.xlsx', :type => "application/vnd.openxmlformates-officedocument.spreadsheetml.sheet"
-				 }
-			end	
-		end
+    if xlsx_request?
+      respond_to do |format|
+        format.xlsx {
+          send_data @org_admins.to_xlsx.to_stream.read, :filename => 'admins.xlsx', :type => "application/vnd.openxmlformates-officedocument.spreadsheetml.sheet"
+         }
+      end 
+    end
 
-		@org_admins		
-	 end
+    @org_admins   
+   end
 
-	def show
-		@org_admin = OrgAdmin.find(params[:id])
-	end
+  def show
+    @org_admin = OrgAdmin.find(params[:id])
+  end
 
-	def update
-		@org_admin = OrgAdmin.find(params[:id])
-		if @org_admin.update permitted_params.org_admin_params().merge update_params
-			render "api/v1/org_admin/show"
-		else
-			respond_with @org_admin
-		end
-	end
+  def update
+    @org_admin = OrgAdmin.find(params[:id])
+    if @org_admin.update permitted_params.org_admin_params().merge update_params
+      render "api/v1/org_admin/show"
+    else
+      respond_with @org_admin
+    end
+  end
 
-	def create
-		@org_admin = OrgAdmin.new
-		params = permitted_params.org_admin_params().merge create_params
-		if @org_admin.update params
-			render "api/v1/org_admin/show"
-		else
-			respond_with @org_admin
-		end
-	end
+  def create
+    @org_admin = OrgAdmin.new
+    params = permitted_params.org_admin_params().merge create_params
+    if @org_admin.update params
+      render "api/v1/org_admin/show"
+    else
+      respond_with @org_admin
+    end
+  end
 
-	def destroy
-		org_admin = OrgAdmin.find(params[:id])
-		org_admin.destroy()
-		render json: {}
-	end
+  def destroy
+    org_admin = OrgAdmin.find(params[:id])
+    org_admin.destroy()
+    render json: {}
+  end
 
 
-	private
+  private
 
-		#virtual params on update
-		def update_params
-			{ password: params[:password], password_confirmation: params[:password_confirmation] }
-		end
+    #virtual params on update
+    def update_params
+      { password: params[:password], password_confirmation: params[:password_confirmation] }
+    end
 
-		#virtual params on create
-		def create_params
-			org = Organisation.find_by(title: params[:admin_for])  	  if current_user.app_admin?
-			org = current_user.admin_for 							  if current_user.org_admin?
+    #virtual params on create
+    def create_params
+      org = Organisation.find_by(title: params[:admin_for])     if current_user.app_admin?
+      org = current_user.admin_for                if current_user.org_admin?
 
-			{ created_by: current_user, admin_for: org, is_active: false, 
-				last_login: Time.zone.now, password: params[:password], 
-				password_confirmation: params[:password_confirmation] }
-		end
+      { created_by: current_user, admin_for: org, is_active: false, 
+        last_login: Time.zone.now, password: params[:password], 
+        password_confirmation: params[:password_confirmation] }
+    end
 end
  
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,30 @@
 class User < ActiveRecord::Base
-	authenticates_with_sorcery!
-	belongs_to :created_by, class_name: "User", foreign_key: "created_by"
-	has_many :users, class_name: "User", foreign_key: "created_by"
+  authenticates_with_sorcery!
+  belongs_to :created_by, class_name: "User", foreign_key: "created_by"
+  has_many :users, class_name: "User", foreign_key: "created_by"
   has_many :activities
 
-	validates_uniqueness_of :email
-	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-	validates_presence_of :email, :first_name, :surname
+  validates_uniqueness_of :email
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates_presence_of :email, :first_name, :surname
 
   validates :password, confirmation: true, length: { in: 6..20 }, :on => :create
-	validates :password_confirmation, presence: true, :on => :create
+  validates :password_confirmation, presence: true, :on => :create
 
-	validates :password, confirmation: true, length: { in: 6..20 }, :allow_blank => true, :on => :update
-	validates :password_confirmation, presence: true, :allow_blank => true, :on => :update
+  validates :password, confirmation: true, length: { in: 6..20 }, :allow_blank => true, :on => :update
+  validates :password_confirmation, presence: true, :allow_blank => true, :on => :update
 
   generate_scopes
 
   def full_name
-      "#{first_name} #{surname}"
+    "#{first_name} #{surname}"
+  end
+
+  def self.search_range(months_ago, date_attr_key)
+    date_attr_str = date_attr_key == :created_at ? "created_at" : "updated_at"
+    if ["3", "6", "9", "12", "24", "36", "48", "60"].include? months_ago
+      (self.send("#{date_attr_str}_lteq", Time.zone.now) & self.send("#{date_attr_str}_gteq", Time.zone.now.ago((months_ago.to_i).months))) 
+    end   
   end
 
 end
